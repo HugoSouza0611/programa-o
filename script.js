a866c645fe8aa02f1c3522e00d0b28488b29a2ed
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-08-13 09:34:15 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-const caixaPrincipal=document.querySelector( '.caixa-principal')
-const caixaPerguntas=document.querySelector( '.caixa-perguntas')
-const caixaAlternativas=document.querySelector( '.caixa-alternativas')
-const caixaResultado=document.querySelector( '.caixa-resultado')
-const textoResultado=document.querySelector( '.caixa-resultado')
+const caixaPrincipal=document.querySelector( '.caixa-principal');
+const caixaPerguntas=document.querySelector( '.caixa-perguntas');
+const caixaAlternativas=document.querySelector( '.caixa-alternativas');
+const caixaResultado=document.querySelector( '.caixa-resultado');
+const textoResultado=document.querySelector( '.caixa-resultado');
+
+const perguntas = [
+    { enunciado: "No âmbito social, você prefere:"
+        alternativas:[
+
+     }
+        
+    { 
+    texto:"Alternativa 1",
+        afirmação: "afirmação",   }
+        { texto: "Alternativa 2",
+        afirmação: "afirmação",
+
+    }
+]
+        {  enunciado: "No âmbito ambietal, você prefere:"
+        alternativas: [
+
+     }
+        
+    { texto: "Alternativa 3",
+        afirmação: "afirmação",
+
+    } { texto: "alternativa 4",
+        afirmação: "afirmação",
+    }
+
+]
+

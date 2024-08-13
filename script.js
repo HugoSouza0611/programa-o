57235d0481aeacf6bdda44cdef4af523a47706ea
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-08-13 10:10:04 -0300

    g

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
-const caixaPrincipal=document.querySelector( '.caixa-principal');
-const caixaPerguntas=document.querySelector( '.caixa-perguntas');
-const caixaAlternativas=document.querySelector( '.caixa-alternativas');
-const caixaResultado=document.querySelector( '.caixa-resultado');
-const textoResultado=document.querySelector( '.caixa-resultado');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [
-    { enunciado: "No âmbito social, você prefere:"
-        alternativas:[
-
-     }
-        
-    { 
-    texto:"Alternativa 1",
-        afirmação: "afirmação",   }
-        { texto: "Alternativa 2",
-        afirmação: "afirmação",
-
-    },
-]
-        {  enunciado: "No âmbito ambietal, você prefere:"
+    {
+        enunciado: "No âmbito social, você prefere:",
         alternativas: [
 
-     }
-        
-    { texto: "Alternativa 3",
-        afirmação: "afirmação",
+            {
+                texto: "Alternativa 1",
+                afirmação: "afirmação",
+            },
+            {
+                texto: "Alternativa 2",
+                afirmação: "afirmação",
 
-    } { texto: "alternativa 4",
-        afirmação: "afirmação",
+            },
+        ]
     },
+    {
+        enunciado: "No âmbito ambiental, você prefere:",
+        alternativas: [
 
+            {
+                texto: "Alternativa 3",
+                afirmação: "afirmação",
+            },
+            {
+                texto: "Alternativa 4",
+                afirmação: "afirmação",
+
+            },
+        ]
+    },
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
+        alternativas: [
+
+            {
+                texto: "Alternativa 5",
+                afirmação: "afirmação",
+            },
+            {
+                texto: "Alternativa 6",
+                afirmação: "afirmação",
+
+            },
+        ]
+    },
 ]
-{  enunciado: "No âmbito tecnológico, você prefere:"
-alternativas: [
 
-}
-
-{ texto: "Alternativa 5",
-afirmação: "afirmação",
-
-} { texto: "alternativa 6",
-afirmação: "afirmação",
-]
-},
-]
-
-let atual= 0
+let atual = 0
 let perguntaAtual;
-function mostraPerguntas(){
-    perguntaAtual= perguntas[atual];
-    caixaPerguntas.textContent= perguntaAtual.enunciado;
+function mostraPerguntas() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 mostraPerguntas();
